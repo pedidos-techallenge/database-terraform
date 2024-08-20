2b7564eb024708629b9e6dfe515451194b224bca
Jubel Cassio <jubelcassioverridasilva@gmail.com>
2024-08-20 00:59:00 +0000

    Criando artefatos do terraform

---
+++ mysql/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "us-east-1"
+    profile = "default"
+}
+
+# TODO: create a resource for retrieving password from secret manager
+
+resource "aws_db_instance" "test_mysql" {
+    engine = "mysql"
+    identifier = "test-mysql"
+    allocated_storage = 20
+    engine_version = "8.0.35"
+    instance_class = "db.t3.small"
+    username = "admin"
+    password = "<placeholder>"
+    skip_final_snapshot = true
+    publicly_accessible = false
+    multi_az = false
+    db_name = "test_db"
+    port = 3306
+}
