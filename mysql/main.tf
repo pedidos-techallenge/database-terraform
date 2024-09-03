0ecc288a97be7c03fb6163d9f908a8ab857a58f6
Jubel Cassio <jubelcassioverridasilva@gmail.com>
2024-09-03 00:49:53 +0000

    ci: :bricks: Setup infra dependencies (vpc, security groups) on main.tf

--- mysql/main.tf
+++ mysql/main.tf
@@ -6,6 +6,34 @@ provider "aws" {}
 
 # TODO: create a resource for retrieving password from secret manager
 
+# Referencing project's vpc to use on other resources
+data "aws_vpc" "techchallenge-vpc" {
+  filter {
+    name = "tag:Name"
+    values = ["techchallenge-vpc"]
+  }
+}
+
+# Referencing private subnets
+data "aws_subnets" "private-subnets" {
+  filter {
+    name = "vpc-id"
+    values = [data.aws_vpc.techchallenge-vpc.id]
+  }
+
+  filter {
+    name = "tag:Name"
+    values = ["*private*"]
+  }
+}
+
+# RDS subnets must be in a sg group
+resource "aws_db_subnet_group" "rds_subnets" {
+  name = "rds_subnets"
+  subnet_ids = data.aws_subnets.private-subnets.ids
+}
+
+# Creating RDS instance
 resource "aws_db_instance" "test_mysql" {
   engine              = "mysql"
   identifier          = "test-mysql"
@@ -19,4 +47,33 @@ resource "aws_db_instance" "test_mysql" {
   multi_az            = false
   db_name             = "test_db"
   port                = 3306
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnets.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+}
+
+
+# Referencing eks cluster
+data "aws_eks_cluster" "techchallenge-eks-cluster" {
+  name = "techchallenge-eks-cluster"
+}
+
+# Security group for tunneling communication from EKS to RDS
+resource "aws_security_group" "rds_sg" {
+  name   = "rds_sg"
+  vpc_id = data.aws_vpc.techchallenge-vpc.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [data.aws_eks_cluster.techchallenge-eks-cluster.vpc_config[0].cluster_security_group_id]
+  }
+
+  # TODO: Make more restrictive
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
